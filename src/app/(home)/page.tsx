1c6d661aa57de098537e4e17c19845f8700e03e5
abhishek martcues <abhishek@martcues.in>
2025-11-07 12:49:08 +0530

    fix

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,27 +1,11 @@
 import HomePage from "@/components/Home";
-import { createTimeFrameExtractor } from "@/utils/timeframe-extractor";
 
 
-type PropsType = {
-  searchParams: Promise<{
-    selected_time_frame?: string;
-  }>;
-};
-
-export default async function Home({ searchParams }: PropsType) {
-  const { selected_time_frame } = await searchParams;
-  const extractTimeFrame = createTimeFrameExtractor(selected_time_frame);
-
-  // Call it on server
-  const paymentsOverviewTimeFrame = extractTimeFrame("payments_overview")?.split(":")[1];
-  const weeksProfitTimeFrame = extractTimeFrame("weeks_profit")?.split(":")[1];
+export default async function Home() {
 
   return (
     <>
-      <HomePage 
-      paymentsOverviewTimeFrame={paymentsOverviewTimeFrame}
-      weeksProfitTimeFrame={weeksProfitTimeFrame} 
-      />
+      <HomePage />
     </>
   );
 }
